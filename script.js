aafd75799cdd5598066d7623ae1030e98bea6e40
Guillaume Herold <guillaumeherold@gmail.com>
2024-04-15 15:03:30 +0200

    début

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Ici votre code JS
+
+    //grid creation
+    function makeGrid() {
+        let tbl = document.getElementById("gamegrid");
+
+        for(let i = 0; i < 20; i++){
+            let myRow = document.createElement("tr");
+            myRow.id = "row" +i;
+            
+            tbl.appendChild(myRow);
+            let roww = document.getElementById("row" + i);
+
+            for(let j = 0; j < 20; j++) {
+                let myCell = document.createElement("td");
+
+                roww.appendChild(myCell)
+            }
+        }
+    }
+    makeGrid();
 });
